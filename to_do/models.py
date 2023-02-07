b615f0f53dfaf79c771cbefe17e760fe57230ae3
ccp84 <ccp84@icloud.com>
2023-02-07 13:49:23 +0000

    py>create model for Item

--- to_do/models.py
+++ to_do/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(blank=False, null=False, max_length=50)
+    done = models.BooleanField(blank=False, null=False, default=False)
