12918eb0132445b0a49b6186d1d0131f6872f44b
ccp84 <ccp84@icloud.com>
2023-02-10 10:50:23 +0000

    py>add delete functionality

--- todo/urls.py
+++ todo/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from to_do.views import get_todo_list, add_item, edit_item
+from to_do import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', get_todo_list, name='get_todo_list'),
-    path('add', add_item, name='add_item'),
-    path('edit/<item_id>', edit_item, name='edit_item')
+    path('', views.get_todo_list, name='get_todo_list'),
+    path('add', views.add_item, name='add_item'),
+    path('edit/<item_id>', views.edit_item, name='edit_item'),
+    path('toggle/<item_id>', views.toggle_item, name='toggle_item'),
+    path('delete/<item_id>', views.delete_item, name='delete_item')
 ]
